627ba4826f4775c833dfea6442f8062353afee80
Dark Tigeuz <ychevessier@etu.u-bordeaux.fr>
2018-12-14 18:36:48 +0100

    Remove useless files

--- Medecin.initialisation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('Medecin.class.php');
-
-$medecin1 = new Medecin("CHE", "yoyo", "45648656", "chaussette");
-$medecin1 -> affiche();
-
-
-?>
--- Patient.initialisation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('Patient.class.php');
-
-$patient1 = new Patient("LEDUC", "Sébastien", "3", "Yeux");
-$patient1 -> affiche();
-
-
-?>
